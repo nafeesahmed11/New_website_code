7dc9bccef3aef6dcc609f2d282975a427cad817b
Nafees_Ahmed <nafesahmedd@gmail.com>
2025-03-15 21:26:19 +0600

    new js

---
+++ lurning projects/java script/array/new_arry.js
@@ -0,0 +1,4 @@
+const fruits=["banana","Apple","orange",];
+// console.log(fruits.pop());
+let x=fruits.push('mango' );
+console.log(x);
---
+++ lurning projects/java script/array/unshift.js
@@ -0,0 +1,5 @@
+const fruits=["banana","Apple","orange",];
+// console.log(fruits.pop());
+fruits.unshift('mango' );
+console.log(fruits);
+
