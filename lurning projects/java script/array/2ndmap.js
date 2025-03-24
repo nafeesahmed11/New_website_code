5889801edb0ae52460506bbac0a350fad0fd458f
Nafees_Ahmed <nafesahmedd@gmail.com>
2025-03-24 11:44:30 +0600

    new js

---
+++ lurning projects/java script/array/2ndmap.js
@@ -0,0 +1,3 @@
+const numbers = [5, 10, 15];
+const newArray = numbers.map((num, index) => `Index ${index}: ${num * 2}`);
+console.log(newArray);
---
+++ lurning projects/java script/array/map.js
@@ -0,0 +1,13 @@
+// const number = [1,2,3,4,5,6,7,8];
+// const duble = number.map(num =>num*2);
+// console.log(duble);
+
+const user = [
+ { id:1, name:'mafuz'},
+ { id:2, name:'rofiq'},
+ { id:3, name:'sheak'},
+];
+
+const userName = user.map(user => user.name);
+console.log(userName);
+
