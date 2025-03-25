5b5128112fecdd946df95ade92c1b7d544d3f0f1
Nafees_Ahmed <nafesahmedd@gmail.com>
2025-03-25 19:37:34 +0600

    new js

--- lurning projects/java script/array/2ndmap.js
+++ lurning projects/java script/array/2ndmap.js
@@ -1,3 +1,11 @@
 const numbers = [5, 10, 15];
 const newArray = numbers.map((num, index) => `Index ${index}: ${num * 2}`);
 console.log(newArray);
+
+let x = numbers.filter(check());
+
+function check(age){
+ return age >= 10;
+};
+
+console.log(x);
